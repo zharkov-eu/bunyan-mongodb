ed8dfac6ed86293dd322b590778ff187fb3f74a9
Evgeni Zharkov <zharkov.e.u@gmail.com>
2018-03-06 13:36:10 +0300

    Регенерация d.ts файлов

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="node" />
 import * as stream from "stream";
 export interface IParameters {
-    dbUrl: string;
+    host: string;
+    database: string;
     collection: string;
+    username?: string;
+    password?: string;
+    authDatabase?: string;
     batchSize?: number;
 }
 export default function mongoWritableStream(parameters: IParameters): stream.Writable;
